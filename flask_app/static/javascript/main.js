8cac66ef0d49a9dfcf40ffba630bedd36a423f9b
Sean <sfh1980@yahoo.com>
2023-03-20 16:32:16 -0400

    update 3-20-23

--- flask_app/static/javascript/main.js
+++ flask_app/static/javascript/main.js
@@ -1,4 +1,80 @@
 
+// navigation menu
+
+(() =>{
+
+    const hamburgerBtn = document.querySelector(".hamburger-btn"),
+    navMenu = document.querySelector(".nav-menu"),
+    closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+    hamburgerBtn.addEventListener("click", showNavMenu);
+    closeNavBtn.addEventListener("click", hideNavMenu);
+
+    function showNavMenu(){
+        navMenu.classList.add("open");
+        bodyScrollingToggle();
+    }
+
+    function hideNavMenu(){
+        navMenu.classList.remove("open");
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+
+    function fadeOutEffect(){
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(() =>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        },300)
+    }
+    // attach an event handler to document 
+    document.addEventListener("click", (event) =>{
+        if(event.target.classList.contains('link-item')){
+            /* make sure event.target.hash has value before overriding default behavior */
+            if(event.target.hash !==""){
+                /* prevent default anchor click behavior */
+                event.preventDefault();
+                const hash = event.target.hash;
+                // console.log(hash); 
+                /*  deactivate existing active 'section' */
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+                /*  activate new 'section' */
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+                /* deactivate existing active navigation menu 'link-item' */
+                navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+                /*  if clicked 'link-item' is contained within the navigation menu */
+                if(navMenu.classList.contains("open")){
+                    /* activate new navigation menu 'link-item' */
+                    event.target.classList.add("active", "inner-shadow");
+                    event.target.classList.remove("outer-shadow", "hover-in-shadow");
+                    /* hide navigation menu */
+                    hideNavMenu();
+                    
+                }
+                else{
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item) =>{
+                        if(hash === item.hash){
+                            /*  activate new navigation menu 'link-item' */
+                            item.classList.add("active", "inner-shadow");
+                            item.classList.remove("outer-shadow", "hover-in-shadow");
+                        }
+                    })
+                    fadeOutEffect();
+                }
+                /*  add hash (#) to URL */
+                window.location.hash = hash;
+            }
+        }
+    })
+
+})();
+
+
+
 
 function bodyScrollingToggle(){
     document.body.classList.toggle("stop-scrolling");
@@ -160,3 +236,16 @@ function bodyScrollingToggle(){
     }
 
 })();
+
+// hide all sections except active
+
+(() =>{
+
+    const sections = document.querySelectorAll(".section");
+    sections.forEach((section) =>{
+        if(!section.classList.contains("active")){
+            section.classList.add("hide");
+        }
+    })
+
+})()
